fdf7d6921830829314f59d2d74b40e0c12186539
lucas foglia <lucas_foglia@hotmail.com>
2022-07-19 23:32:08 -0300

    segundo desafio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+       <NavBar/>
       <header className="App-header">
+     
         <img src={logo} className="App-logo" alt="logo" />
         <p>react</p>
         <p>
